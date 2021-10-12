17f973184f9e405b69550020dba0c0274a669903
gigaChester <santiagohsosa@gmail.com>
2021-10-11 22:35:27 -0300

    modifique app

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
-console.log(process.env.PORT);
+//console.log(process.env.PORT);
 
 const path = require('path');
 const pathPublic = path.join(__dirname, './public');
 app.use(express.static(pathPublic));
 
-app.listen(undefined || 3000, console.log('Servidor OK'));
-app.get('/', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
+//Falta una barra para el OR
+app.listen(process.env.PORT || 3030,() => {console.log("Mercado Liebre funciona")});
+
+//app.listen(undefined || 3000, console.log('Servidor OK'));
+//app.get('/', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
 // app.get('/', (req,res) => {res.sendFile(__dirname + './views/home.html')});
 
 
